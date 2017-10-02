90ed5c6fa8887a2a3444bc0aba4fcd5840cebb86
Mateusz Galas <s0ulhunter@wp.pl>
2017-10-02 13:31:17 +0200

    RTV shop client 1st commit

---
+++ JavaApplication2/src/javaapplication2/TestApplication.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author student
+ */
+class TestApplication {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Client client1 = new Client("jan","kowalski");
+        Client client2 = new Client("andrzej","malinowski");
+        Client client3 = new Client("zbigniew","nowak");
+
+        String aa = client1.getName();
+        int id1 = client1.getID();
+        int id2 = client2.getID();
+        int id3 = client3.getID();
+
+    
+        System.out.println(aa);
+        System.out.println(id1 + " " + id2 + " " + id3);
+
+    
+    }
+    
+}

b7d558a5154a1af2e7c233864708e88d5277ef18
Mokasyn56 <hubert_b@op.pl>
2017-11-13 15:54:44 +0100

    Klient

--- Hotel/src/hotel/Client.java
+++ Hotel/src/hotel/Client.java
@@ -9,6 +9,78 @@ package hotel;
  *
  * @author PC
  */
-public class Client {
-    
+abstract class Client {
+    public Client(){}
+    public String Name;
+    public String Surname;
+    public int age;
+    public long PESEL;
+    public double discount;
 }
+
+class Person extends Client{
+    public Person(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0;
+    }
+}
+
+class LoyalClient extends Client{
+    public LoyalClient(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0.2;
+    }
+}
+
+class Learner extends Client{
+    public Learner(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0.47;
+    }
+}
+
+class Student extends Client{
+    public Student(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0.4;
+    }
+}
+
+class Pensioner extends Client{
+    public Pensioner(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0.3;
+    }
+}
+
+class Invalid extends Client{
+    public Invalid(String Name, String Surname, int age, long PESEL){
+        super();
+        this.Name = Name;
+        this.Surname = Surname;
+        this.age = age;
+        this.PESEL = PESEL;
+        this.discount = 0.55;
+    }
+}
+

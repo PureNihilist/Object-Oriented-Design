6a3863775d5c6615d7b5e69447cd5adecc33aaec
Mokasyn56 <hubert_b@op.pl>
2017-11-18 11:30:43 +0100

    Edited period and added Menu

---
+++ Hotel/src/hotel/Menu.java
@@ -0,0 +1,24 @@
+public class Menu{
+    public static void main(String[] args) {
+        System.out.println("Witamy w systemie obsługi hotelu recepcja!");
+        System.out.println();
+        System.out.println("1. Wyświetl wszystkie rezerwacje.");
+        System.out.println("2. Dodaj nową rezerwację.");
+        System.out.println("3. Wyszukaj wolne pokoje.");
+        System.out.println("4. Zakończ działanie systemu.");
+        Scanner reader = new Scanner(System.in);
+        int choice = reader.nextInt();
+        switch (choice) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+                System.exit(0);
+    }
+}
